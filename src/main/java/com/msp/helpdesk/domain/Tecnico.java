6cb51f34ce29ba20b77bfa64c86336706f4ef907
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2023-05-19 19:16:46 -0400

    Endpoint create para Tecnico

--- src/main/java/com/msp/helpdesk/domain/Tecnico.java
+++ src/main/java/com/msp/helpdesk/domain/Tecnico.java
@@ -3,11 +3,13 @@ package com.msp.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.msp.helpdesk.domain.dtos.TecnicoDTO;
 import com.msp.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -32,6 +34,17 @@ public class Tecnico extends Pessoa{
 		addPerfis(Perfil.CLIENTE);
 	}
 
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+	
 	public List<Chamado> getChamados() {
 		return chamados;
 	}

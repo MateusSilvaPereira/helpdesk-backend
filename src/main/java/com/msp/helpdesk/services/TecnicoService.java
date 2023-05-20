0f62c517eb31035bacf56686179dbed11fc1ea88
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2023-05-20 16:04:22 -0400

    Metodo PUT para Tecnico implementado

--- src/main/java/com/msp/helpdesk/services/TecnicoService.java
+++ src/main/java/com/msp/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.msp.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,16 +23,16 @@ public class TecnicoService {
 	private TecnicoRepository repository;
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado! id: " + id));
 	}
-	
-	public List<Tecnico> findAll(){
+
+	public List<Tecnico> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
 		validaCpfEEmail(objDTO);
@@ -38,17 +40,23 @@ public class TecnicoService {
 		return repository.save(newObj);
 	}
 
-	private void validaCpfEEmail(TecnicoDTO objDTO) {
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validaCpfEEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
+	}
 
+	private void validaCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
-		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
 		}
 		obj = pessoaRepository.findByEmail(objDTO.getEmail());
-	
-		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
 		}
 	}
-	
-} 
+}

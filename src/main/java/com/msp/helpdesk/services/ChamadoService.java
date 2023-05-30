8e383aede205fd3dbe3af93e13215eb4abbcff1d
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2023-05-30 07:39:10 -0400

    Create para Chamado

--- src/main/java/com/msp/helpdesk/services/ChamadoService.java
+++ src/main/java/com/msp/helpdesk/services/ChamadoService.java
@@ -3,16 +3,29 @@ package com.msp.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.msp.helpdesk.domain.Chamado;
+import com.msp.helpdesk.domain.Cliente;
+import com.msp.helpdesk.domain.Tecnico;
+import com.msp.helpdesk.domain.dtos.ChamadoDTO;
+import com.msp.helpdesk.domain.enums.Prioridade;
+import com.msp.helpdesk.domain.enums.Status;
 import com.msp.helpdesk.repositories.ChamadoRepository;
 import com.msp.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ChamadoService {
-
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@Autowired
+	private TecnicoService tecnicoService;
+	
 	@Autowired
 	private ChamadoRepository repository;
 	
@@ -27,4 +40,39 @@ public class ChamadoService {
 		List<Chamado> list = repository.findAll();
 		return list;
 	}
+
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+

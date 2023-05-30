d7f37ce1141decaacb180edd9de55ffa6c5c44ee
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2023-05-30 08:14:54 -0400

    Update para Chamado implementado

--- src/main/java/com/msp/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/msp/helpdesk/resources/ChamadoResource.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,86 +29,29 @@ public class ChamadoResource {
 	private ChamadoService service;
 
 	@GetMapping("/{id}")
-	private ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+	private ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
 		Chamado obj = service.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
 	}
-	
-	
+
 	@GetMapping
-	public ResponseEntity<List<ChamadoDTO>> findAll(){
+	public ResponseEntity<List<ChamadoDTO>> findAll() {
 		List<Chamado> list = service.findAll();
 		List<ChamadoDTO> listDTO = list.stream().map(x -> new ChamadoDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO) {
 		Chamado newObj = service.create(objDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
-	return ResponseEntity.created(uri).build(); 
+		return ResponseEntity.created(uri).build();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	@PutMapping("/{id}")
+	public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id, @Valid @RequestBody ChamadoDTO objDTO) {
+		Chamado newObj = service.update(id, objDTO);
+		return ResponseEntity.ok().body(new ChamadoDTO(newObj));
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/main/java/com/msp/helpdesk/services/ChamadoService.java
+++ src/main/java/com/msp/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.msp.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,42 +20,51 @@ import com.msp.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ChamadoService {
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private TecnicoService tecnicoService;
-	
+
 	@Autowired
 	private ChamadoRepository repository;
-	
-	
+
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
 	}
-
 
 	public List<Chamado> findAll() {
 		List<Chamado> list = repository.findAll();
 		return list;
 	}
 
-
 	public Chamado create(@Valid ChamadoDTO objDTO) {
 		return repository.save(newChamado(objDTO));
 	}
-	
+
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+
+		return repository.save(oldObj);
+	}
+
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
 		Cliente cliente = clienteService.findById(obj.getCliente());
-		
+
 		Chamado chamado = new Chamado();
-		if(obj.getId() != null) {
+		if (obj.getId() != null) {
 			chamado.setId(obj.getId());
 		}
 		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
+		}
+
 		chamado.setTecnico(tecnico);
 		chamado.setCliente(cliente);
 		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
@@ -63,16 +73,5 @@ public class ChamadoService {
 		chamado.setObservacoes(obj.getObservacoes());
 		return chamado;
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-

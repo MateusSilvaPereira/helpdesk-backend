061ea8e76e8834fb29477abe88111dbe18bfc1fb
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2023-09-04 15:12:24 -0400

    Teste para class ChamadoService Criada

---
+++ src/test/java/com/msp/helpdesk/services/ChamadoServiceTest.java
@@ -0,0 +1,29 @@
+package com.msp.helpdesk.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChamadoServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+}
